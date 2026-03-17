90c26b0977bfcd11fb836208eb49704e3ad66571
VinitSadhanapada <vinit.nikam-ext@sadhguru.org>
2026-03-17 22:34:54 +1100

    Initial commit: Awareness Challenge leaderboard

---
+++ leaderboard_data.js
@@ -0,0 +1,323 @@
+const LEADERBOARD_DATA = {
+  "lastUpdated": "17 Mar 2026, 10:20 PM",
+  "teams": [
+    {
+      "name": "The Crocs",
+      "emoji": "🐊",
+      "totalPoints": 12,
+      "members": [
+        {
+          "name": "Haritha",
+          "points": 2
+        },
+        {
+          "name": "Nala",
+          "points": 2
+        },
+        {
+          "name": "Vedang",
+          "points": 2
+        },
+        {
+          "name": "Viji",
+          "points": 2
+        },
+        {
+          "name": "Abhishek",
+          "points": 1
+        },
+        {
+          "name": "Divya",
+          "points": 1
+        },
+        {
+          "name": "Ewan",
+          "points": 1
+        },
+        {
+          "name": "Wini",
+          "points": 1
+        },
+        {
+          "name": "Fun",
+          "points": 0
+        },
+        {
+          "name": "Sau",
+          "points": 0
+        },
+        {
+          "name": "Sumi",
+          "points": 0
+        }
+      ]
+    },
+    {
+      "name": "The Koalas",
+      "emoji": "🐨",
+      "totalPoints": 6,
+      "members": [
+        {
+          "name": "Anju",
+          "points": 1
+        },
+        {
+          "name": "Gurasis",
+          "points": 1
+        },
+        {
+          "name": "Manju",
+          "points": 1
+        },
+        {
+          "name": "Ramky",
+          "points": 1
+        },
+        {
+          "name": "Shine",
+          "points": 1
+        },
+        {
+          "name": "Vedant",
+          "points": 1
+        },
+        {
+          "name": "Aadesh",
+          "points": 0
+        },
+        {
+          "name": "Daventure",
+          "points": 0
+        },
+        {
+          "name": "Reshmi",
+          "points": 0
+        },
+        {
+          "name": "Sonal",
+          "points": 0
+        },
+        {
+          "name": "Sumit",
+          "points": 0
+        },
+        {
+          "name": "Vasu",
+          "points": 0
+        }
+      ]
+    },
+    {
+      "name": "The Kangaroos",
+      "emoji": "🦘",
+      "totalPoints": 5,
+      "members": [
+        {
+          "name": "Ankur",
+          "points": 2
+        },
+        {
+          "name": "Vinit",
+          "points": 2
+        },
+        {
+          "name": "Sajja",
+          "points": 1
+        },
+        {
+          "name": "Arturo",
+          "points": 0
+        },
+        {
+          "name": "Christy",
+          "points": 0
+        },
+        {
+          "name": "Daniel",
+          "points": 0
+        },
+        {
+          "name": "Kripa",
+          "points": 0
+        },
+        {
+          "name": "Neha",
+          "points": 0
+        },
+        {
+          "name": "Sangita",
+          "points": 0
+        },
+        {
+          "name": "Srirekha",
+          "points": 0
+        },
+        {
+          "name": "Susan",
+          "points": 0
+        },
+        {
+          "name": "Vaibhav",
+          "points": 0
+        }
+      ]
+    },
+    {
+      "name": "The Sharks",
+      "emoji": "🦈",
+      "totalPoints": 4,
+      "members": [
+        {
+          "name": "Lalit",
+          "points": 2
+        },
+        {
+          "name": "Abhinandan",
+          "points": 1
+        },
+        {
+          "name": "Mangalam",
+          "points": 1
+        },
+        {
+          "name": "Ashish",
+          "points": 0
+        },
+        {
+          "name": "Dhanyasree",
+          "points": 0
+        },
+        {
+          "name": "Kanak",
+          "points": 0
+        },
+        {
+          "name": "Mimi",
+          "points": 1
+        },
+        {
+          "name": "Nabin",
+          "points": 0
+        },
+        {
+          "name": "Prakash",
+          "points": 0
+        },
+        {
+          "name": "Roopal",
+          "points": 0
+        },
+        {
+          "name": "Sulochana",
+          "points": 0
+        },
+        {
+          "name": "Sunidhi",
+          "points": 0
+        }
+      ]
+    },
+    {
+      "name": "The Flying Foxes",
+      "emoji": "🦇",
+      "totalPoints": 3,
+      "members": [
+        {
+          "name": "Richaa",
+          "points": 2
+        },
+        {
+          "name": "Orin",
+          "points": 1
+        },
+        {
+          "name": "61 405 266 936",
+          "points": 0
+        },
+        {
+          "name": "Aditya",
+          "points": 0
+        },
+        {
+          "name": "Ashina",
+          "points": 0
+        },
+        {
+          "name": "Deepak",
+          "points": 0
+        },
+        {
+          "name": "Deepti",
+          "points": 0
+        },
+        {
+          "name": "Dhiraj",
+          "points": 0
+        },
+        {
+          "name": "Grand",
+          "points": 0
+        },
+        {
+          "name": "Latha",
+          "points": 0
+        },
+        {
+          "name": "Olivia",
+          "points": 0
+        }
+      ]
+    },
+    {
+      "name": "The Dingoes",
+      "emoji": "🐺",
+      "totalPoints": 2,
+      "members": [
+        {
+          "name": "Vennila",
+          "points": 2
+        },
+        {
+          "name": "Harprett",
+          "points": 0
+        },
+        {
+          "name": "Hiten",
+          "points": 0
+        },
+        {
+          "name": "Kaori",
+          "points": 0
+        },
+        {
+          "name": "Lakshmi",
+          "points": 0
+        },
+        {
+          "name": "Mahesh",
+          "points": 0
+        },
+        {
+          "name": "Manjeet",
+          "points": 0
+        },
+        {
+          "name": "Meena",
+          "points": 1
+        },
+        {
+          "name": "Ravi",
+          "points": 0
+        },
+        {
+          "name": "Rojita",
+          "points": 0
+        },
+        {
+          "name": "Uday",
+          "points": 0
+        }
+      ]
+    }
+  ]
+};
